0da8d8c55d3d8d72563fccded09314bf199f1fb2
Thokozani XABA <txaba@student.wethinkcode.co.za>
2019-06-12 10:36:32 +0200

    ft_strdup

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: txaba <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/20 09:12:27 by txaba             #+#    #+#             */
-/*   Updated: 2019/05/20 12:18:02 by txaba            ###   ########.fr       */
+/*   Updated: 2019/05/28 12:02:35 by txaba            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,24 +15,14 @@
 char	*ft_strdup(const char *str)
 {
 	char	*d;
-	size_t	i;
-	size_t	j;
 	size_t	len;
 
-	i = 0;
-	j = 0;
 	len = ft_strlen(str);
 	d = (char *)malloc(sizeof(char) * (len + 1));
 	if (d == NULL)
 	{
 		return (NULL);
 	}
-	while (str[i])
-	{
-		d[j] = str[i];
-		j++;
-		i++;
-	}
-	d[j] = '\0';
+	ft_strcpy(d, str);
 	return (d);
 }
