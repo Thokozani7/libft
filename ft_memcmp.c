7492c91147b80c2424e04710a0699de5b841e995
Thokozani XABA <txaba@student.wethinkcode.co.za>
2019-06-12 10:34:58 +0200

    ft_memcmp

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: txaba <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/21 15:34:07 by txaba             #+#    #+#             */
-/*   Updated: 2019/05/21 16:32:32 by txaba            ###   ########.fr       */
+/*   Updated: 2019/05/28 17:25:49 by txaba            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,17 @@
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	size_t	i;
-	char	*str1;
-	char	*str2;
+	size_t			i;
+	unsigned char	*str1;
+	unsigned char	*str2;
 
 	i = 0;
-	str1 = (char *)s1;
-	str2 = (char *)s2;
-	while (str1[i] && str2[i] && str1[i] == str2[i] && i < (n - 1))
+	str1 = (unsigned char *)s1;
+	str2 = (unsigned char *)s2;
+	if (n == 0)
+		return (0);
+	while (str1[i] == str2[i] && i < (n - 1))
 	{
-		str1[i] = str1[i];
 		i++;
 	}
 	return (str1[i] - str2[i]);
