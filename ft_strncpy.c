8013a838af8faf11f07408b207c2add8990864e4
Thokozani XABA <txaba@student.wethinkcode.co.za>
2019-06-12 11:04:20 +0200

    ft_strncpy

--- ft_strncpy.c
+++ ft_strncpy.c
@@ -6,7 +6,7 @@
 /*   By: txaba <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/20 12:59:05 by txaba             #+#    #+#             */
-/*   Updated: 2019/05/20 13:01:28 by txaba            ###   ########.fr       */
+/*   Updated: 2019/06/12 11:03:36 by txaba            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,21 @@
 
 char	*ft_strncpy(char *dest, const char *src, size_t n)
 {
-	int	i;
+	size_t	i;
+	size_t	a;
 
 	i = 0;
-	while (src[i] && i < n)
+	while (src[i] != '\0' && (i < n))
 	{
 		dest[i] = src[i];
 		i++;
 	}
-	dest[i] = '\0';
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	a = ft_strlen(dest) - i;
+	dest[i + a] = '\0';
 	return (dest);
 }
