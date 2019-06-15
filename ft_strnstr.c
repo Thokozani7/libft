521afc76301c744ed382faec044a092f8e3bf2e5
Thokozani XABA <txaba@student.wethinkcode.co.za>
2019-06-15 11:24:18 +0200

    ft_strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,26 +6,28 @@
 /*   By: txaba <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/21 10:58:02 by txaba             #+#    #+#             */
-/*   Updated: 2019/05/21 15:04:38 by txaba            ###   ########.fr       */
+/*   Updated: 2019/06/13 08:56:19 by txaba            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+char	*ft_strnstr(const char *ha, const char *ne, size_t len)
 {
 	size_t	i;
-	int		j;
+	size_t	j;
 	char	*hay;
 	char	*nee;
 
 	i = 0;
-	hay = (char *)haystack;
-	nee = (char *)needle;
+	hay = (char *)ha;
+	nee = (char *)ne;
+	if (nee[i] == '\0')
+		return (hay);
 	while (hay[i] && i < len)
 	{
 		j = 0;
-		while (hay[i + j] == nee[j])
+		while (hay[i + j] == nee[j] && (i + j) < len)
 		{
 			j++;
 			if (nee[j] == '\0')
