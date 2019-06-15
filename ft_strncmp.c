afb28295840dcd75d8127bcb54bdcc0df2e73184
Thokozani XABA <txaba@student.wethinkcode.co.za>
2019-06-15 11:24:54 +0200

    ft_strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,20 +6,20 @@
 /*   By: txaba <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/20 12:35:51 by txaba             #+#    #+#             */
-/*   Updated: 2019/05/20 12:36:38 by txaba            ###   ########.fr       */
+/*   Updated: 2019/06/05 10:19:27 by txaba            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	strncmp(const char *s1, const char *s2, size_t n)
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	int	i;
+	size_t	i;
 
 	i = 0;
-	while (s1[i] && s1[i] == s2[i] && i > n - 1)
-	{
+	if (n == 0)
+		return (0);
+	while (s1[i] && s2[i] && s1[i] == s2[i] && i < (n - 1))
 		i++;
-	}
-	return (s1[i] - s2[i]);
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
