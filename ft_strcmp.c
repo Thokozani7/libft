d550b883ce19c7055b72bc464f3ab57005215ba7
Thokozani XABA <txaba@student.wethinkcode.co.za>
2019-06-15 11:24:42 +0200

    ft_strcmp

--- ft_strcmp.c
+++ ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: txaba <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/20 12:34:10 by txaba             #+#    #+#             */
-/*   Updated: 2019/05/20 12:35:08 by txaba            ###   ########.fr       */
+/*   Updated: 2019/05/31 16:49:21 by txaba            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,7 @@ int	ft_strcmp(const char *s1, const char *s2)
 	int	i;
 
 	i = 0;
-	while (s1[i] && s1[i] == s2[i])
-	{
+	while (s1[i] && s2[i] && s1[i] == s2[i])
 		i++;
-	}
-	return (s1[i] - s2[i]);
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
